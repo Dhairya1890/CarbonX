bfdada9075533a3f172042118ec9fec6648351df
Dhairya <sharmajikaemail123@gmail.com>
2025-06-02 22:58:00 +0530

    Final commit

--- results.js
+++ results.js
@@ -1,3 +1,13 @@
+
+let badge = document.getElementById("badge");
+let text1 = document.getElementById("text1");
+let text2 = document.getElementById("text2");
+let text3 = document.getElementById("text3");
+let text4 = document.getElementById("text4");
+let text5 = document.getElementById("text5");
+const button = document.getElementById("Resultbutton");
+const popup = document.getElementById("results-popup");
+const warn = document.getElementById("warning");
 //Evaluation System
 document.getElementById("Resultbutton").addEventListener("click", function(){
 let Q1 = parseInt(document.getElementById("transportation").value);
@@ -12,5 +22,61 @@ let Q9 = parseInt(document.getElementById("public").value);
 let Q10 = parseInt(document.getElementById("waste").value);
     let totalScore = Q1+Q2+Q3+Q4+Q5+Q6+Q7+Q8+Q9+Q10;
     console.log(totalScore);
+    popup.classList.add("active");
+
+
+if(totalScore <=116){
+    badge.src = "medal1.png";
+    text1.innerHTML = 'You are a <span class="bold">Eco-Champion!</span>';
+    text2.innerHTML = 'You only emit: <span class="bold">1-2 tons CO<sub>2</sub>/year</span>';
+    text3.innerHTML = 'If everyone lived like you: <span class="bold">~1.2&#176;C</span> or lower temp rise!';
+    text4.innerHTML = '<span class="bold">You are giving this planet a sustainable future!</span>';
+    text5.innerHTML = '<span id="warning">The good part : </span>You&apos;re leading the way toward a sustainable future. At just 1–2 tons/year, your lifestyle is proof that low-impact living is possible — and powerful. Stay strong and inspire others</p>'
+}
+else if(totalScore<=415){
+ badge.src = "medal2.png";
+    text1.innerHTML = 'You are an <span class="bold">Eco-Advocate!</span>';
+    text2.innerHTML = 'You emit around: <span class="bold">3-4 tons CO<sub>2</sub>/year</span>';
+    text3.innerHTML = 'If everyone lived like you: <span class="bold">~1.8&#176;C</span> temp rise!';
+    text4.innerHTML = '<span class="bold">You\'re making just enough efforts to maintain Sustainability!</span>';
+    text5.innerHTML = '<span id="warning">The good part : </span>You&apos;re on the right path — emitting just 3–5 tons/year helps limit warming to safer levels. Keep optimizing your choices; your effort makes a real difference.'
+}
+else if(totalScore <=700){
+    warn.classList.add("red");
+    badge.src = "medal3.png";
+    text1.innerHTML = 'You are an <span class="bold">Average Global Citizen</span>';
+    text2.innerHTML = 'You emit around: <span class="bold">6-8 tons CO<sub>2</sub>/year</span>';
+    text3.innerHTML = 'If everyone lived like you: <span class="bold">~2.5&#176;-3&#176;C</span> temp rise!';
+    text4.innerHTML = 'Your type of are the most comman, that are a risk to this planet';
+    text5.innerHTML = '<span id="warning">Warning : </span>Even 6–8 tons per year is too much. You may seem average, but you&apos;re still contributing to a 2.5–3°C rise, which brings severe heatwaves, floods, and a dangerous "new normal." It&apos;s time to shift.'
+}
+
+else if(totalScore <=900){
+    warn.classList.add("red");
+    badge.src = "medal4.png";
+    text1.innerHTML = 'You are an <span class="bold">Carbon Heavyweight</span>';
+    text2.innerHTML = 'You emit around: <span class="bold">15-25 tons CO<sub>2</sub>/year</span>';
+    text3.innerHTML = 'If everyone lived like you: <span class="bold">~3.5&#176;-4.5&#176;C</span> temp rise!';
+    text4.innerHTML = 'You are a weight to this planet';
+    text5.innerHTML = '<span id="warning">Warning : </span>At 15–25 tons of CO₂ annually, your impact is driving extreme climate disruption. If you don’t make changes, you’re accelerating irreversible damage to both the planet and future generations.'
+}
+else if(totalScore >= 901){
+    warn.classList.add("red");
+    badge.src = "medal5.png";
+    text1.innerHTML = 'You are an <span class="bold">Unaware Contributor</span>';
+    text2.innerHTML = 'You emit around: <span class="bold">20+ tons CO<sub>2</sub>/year</span>';
+    text3.innerHTML = 'If everyone lived like you: <span class="bold">~4&#176;- 5&#176;C</span> temp rise!';
+    text4.innerHTML = 'You are unaware of real problems, Probably you are from Reel Gen';
+    text5.innerHTML = '<span id="warning">Warning : </span>You need to become conscious of your lifestyle — emitting 10–20+ tons of CO₂ a year is pushing us toward catastrophic 4–5°C global warming. At that point, systems collapse — food, water, and safety.'
+
+}
 })
+
+
+
+
+
+
+
+
 
--- script.js
+++ script.js
@@ -1,307 +1,145 @@
-document.getElementById("popup-close-button").onclick = function() {
-    document.getElementById("popup").style.display = "block";
-};
-
-document.querySelector(".close").onclick = function(){
-    document.getElementById("popup").style.display = "none";
-};
-
-document.getElementById("popup-close-button").onclick = function(){
-    document.getElementById("popup").style.display = "none"; 
-};
-
-window.onload = function () {
-   document.getElementById("popup").style.display = "block";
-};
-
-const translation = {
-    en : {
-        title : "IF EVERYONE LIVED LIKE YOU...",
-        sub1 : "See how your lifestyle could shape the future of our planet",
-        sub2 : "How much the global temperature will rise?",
-        sub3 : "Find the X"
-    },
-    in : {
-        title : "🌍अगर हर कोई आपके जैसा रहता...🌍",
-        sub1 : "देखें कि आपकी जीवनशैली पृथ्वी के भविष्य को कैसे प्रभावित कर सकती है",
-        sub2 : "यदि सभी आपकी तरह रहते हैं तो तापमान कितना बढ़ेगा?",
-        sub3 : "X को खोजें"
-    }
-};
-
-const dropdown = document.getElementById("languages");
-
-dropdown.addEventListener('change', function(){
-    const selectedValue = this.value;
-    console.log("User Selected", selectedValue)
-        document.getElementById("heading").textContent = translation[selectedValue].title;
-        document.getElementById("sub-head1").textContent = translation[selectedValue].sub1;
-        document.getElementById("sub-head2").textContent = translation[selectedValue].sub2;
-        document.getElementById("button").textContent = translation[selectedValue].sub3;
-});
-
-//Cursor light
-const light = document.querySelector('.cursor-light');
-
-document.addEventListener('mousemove', (e) => {
-  light.style.left = `${e.clientX}px`;
-  light.style.top = `${e.clientY}px`;
-});
-
-
 
 // Question 1
 const transportSlider = document.getElementById("transportation");
 const Description = document.getElementById("description");
 
 const TransportLevels = [
-  "Never 🥱",
-  "Rarely 🤔",
-  "Sometimes 😊",
-  "Often 😀",
-  "Daily 🤩"
+  "Never",
+  "Rarely",
+  "Sometimes",
+  "Often",
+  "Daily"
 ];
 
 transportSlider.addEventListener("input", function () {
-  const value = parseInt(this.value);
-  Description.textContent = TransportLevels[value];
+  const value = parseInt(this.value);
+  Description.textContent = TransportLevels[value];
 });
 
-//Question 2
+// Question 2
 const airtravelSlider = document.getElementById("airtravel");
 const Description02 = document.getElementById("description01");
 
 const airtravelLevels = [
-  "0 Hours 🤩",
-  "2 to 4 hours 😀",
-  "4 to 5 hours 😊",
-  "6 to 8 hours 😥",
-  "More than 10 hours 😓"
+  "0 Hours",
+  "2 to 4 hours",
+  "4 to 5 hours",
+  "6 to 8 hours",
+  "More than 10 hours"
 ];
 
 airtravelSlider.addEventListener("input", function () {
-  const value = parseInt(this.value);
-  Description02.textContent = airtravelLevels[value];
+  const value = parseInt(this.value);
+  Description02.textContent = airtravelLevels[value];
 });
 
-//Question 3
+// Question 3
 const foodhabitSlider = document.getElementById("foodhabits");
 const Description03 = document.getElementById("description02");
 
 const foodLevels = [
-  "Never 🤩",
-  "Rarely 😀",
-  "Sometimes 😊",
-  "Often 🤔",
-  "Daily 😅"
+  "Never",
+  "Rarely",
+  "Sometimes",
+  "Often",
+  "Daily"
 ];
 
 foodhabitSlider.addEventListener("input", function () {
-  const value = parseInt(this.value);
-  Description03.textContent = foodLevels[value];
+  const value = parseInt(this.value);
+  Description03.textContent = foodLevels[value];
 });
 
-//Question 4
+// Question 4
 const housingSlider = document.getElementById("housing");
 const Description04 = document.getElementById("description03");
 
 const familyLevels = [
-  "Alone 😓",
-  "With my Partner 🥰",
-  "With Family 😊"
+  "Alone",
+  "With my Partner",
+  "With Family"
 ];
 
 housingSlider.addEventListener("input", function () {
-  const value = parseInt(this.value);
-  Description04.textContent = familyLevels[value];
+  const value = parseInt(this.value);
+  Description04.textContent = familyLevels[value];
 });
 
-//Question 5
-
+// Question 5
 const energySlider = document.getElementById("energy");
 const Description05 = document.getElementById("description04");
 
 const energyLevels = [
-"I don’t use any energy-efficient appliances 😓",
-"I turn off lights/appliances when not in use 💡",
-"I use some energy-saving devices (LEDs, star-rated AC, etc.) 🌿",
-"I follow most best practices + use efficient appliances ⚡🏡",
-"My home is optimized for energy saving (solar, smart devices, insulation) ☀️🔋 "
+    "I don’t use any energy-efficient appliances",
+    "I turn off lights/appliances when not in use",
+    "I use some energy-saving devices (LEDs, star-rated AC, etc.)",
+    "I follow most best practices + use efficient appliances",
+    "My home is optimized for energy saving (solar, smart devices, insulation)"
 ];
 
 energySlider.addEventListener("input", function () {
-  const value = parseInt(this.value);
-  Description05.textContent = energyLevels[value];
+  const value = parseInt(this.value);
+  Description05.textContent = energyLevels[value];
 });
 
-//Question 6
-
+// Question 6
 const foodSlider = document.getElementById("food");
 const Description06 = document.getElementById("description05");
 
 const FoodGrowthLevels = [
-"None of the food 😥",
-"Some of the food🙂",
-"Almost all😀",
-"All Food WE EAT ARE LOCAL AND UNPROCESSED 😌"
-]
+    "None of the food",
+    "Some of the food",
+    "Almost all",
+    "All Food WE EAT ARE LOCAL AND UNPROCESSED"
+];
 
 foodSlider.addEventListener("input", function(){
-  const value = parseInt(this.value);
-  Description06.textContent = FoodGrowthLevels[value];
+  const value = parseInt(this.value);
+  Description06.textContent = FoodGrowthLevels[value];
 });
 
-//Question 7
-
+// Question 7
 const GarbageSlider = document.getElementById("garbage");
 const Description07 = document.getElementById("description06");
 
 const GarbageLevels = [
-"Much more than neighbours😥",
-"More than them🙂",
-"Same😀",
-"Less than neighbours 😌"
-]
+    "Much more than neighbours",
+    "More than them",
+    "Same",
+    "Less than neighbours"
+];
 
 GarbageSlider.addEventListener("input", function(){
-  const value = parseInt(this.value);
-  Description07.textContent = GarbageLevels[value];
+  const value = parseInt(this.value);
+  Description07.textContent = GarbageLevels[value];
 });
 
-//Question 8
+// Question 8
 const CarSlider = document.getElementById("car");
 const Description08 = document.getElementById("description07");
 
 CarSlider.oninput = function(){
-  Description08.innerHTML = `${this.value} KM's`;
+  Description08.innerHTML = `${this.value} KM's`;
 };
 
-//Question 9
+// Question 9
 const PublicSlider = document.getElementById("public");
-const Description09 = document.getElementById("description08");
+// FIX: Corrected typo in the assignment for Description09
+const Description09 = document.getElementById("description08"); 
 
 PublicSlider.oninput = function(){
-  Description09.innerHTML = `${this.value} KM's`;
+  Description09.innerHTML = `${this.value} KM's`;
 };
 
-//Question 10
+// Question 10
 const PlasticSlider = document.getElementById("waste");
 const Description010 = document.getElementById("description09");
 
 PlasticSlider.oninput = function(){
-  Description010.innerHTML = `${this.value} Bottles 🥤`;
+  Description010.innerHTML = `${this.value} Bottles`;
 };
 
-document.getElementById("popup-close-button").addEventListener("click", function (){
-    document.getElementById("intro1").style.display = "block";
-    document.getElementById("modal-content").style.display = "none";
-});
-// document.getElementById("back01").addEventListener("click", function(){
-//     document.getElementById("intro1").style.display = "none";
-//     document.getElementById("hero").style.display = "block";
-// });
-document.getElementById("next01").addEventListener("click", function(){
-    document.getElementById("intro1").style.display = "none";
-    document.getElementById("q1").style.display = "block";
-});
-document.getElementById("back02").addEventListener("click", function(){
-    document.getElementById("q1").style.display = "none";
-    document.getElementById("intro1").style.display = "block";
-});
-document.getElementById("back02").addEventListener("click", function(){
-    document.getElementById("questions").classList.add("q1");
-    document.getElementById("hero").style.display = "block";
-})
 
-document.getElementById("next02").addEventListener("click", function(){
-    document.getElementById("q1").style.display = "none";
-    document.getElementById("q2").style.display = "block";
-})
-document.getElementById("back03").addEventListener("click", function(){
-    document.getElementById("q2").style.display = "none";
-    document.getElementById("q1").style.display = "block";
-})
-
-document.getElementById("next03").addEventListener("click", function(){
-    document.getElementById("q2").style.display = "none";
-    document.getElementById("q3").style.display = "block";
-})
-
-document.getElementById("back04").addEventListener("click", function(){
-    document.getElementById("q3").style.display = "none";
-    document.getElementById("q2").style.display = "block";
-})
-
-document.getElementById("next04").addEventListener("click", function(){
-    document.getElementById("q4").style.display = "block";
-    document.getElementById("q3").style.display = "none";
-})
-
-document.getElementById("back05").addEventListener("click", function(){
-    document.getElementById("q3").style.display = "block";
-    document.getElementById("q4").style.display = "none";
-})
-
-document.getElementById("next05").addEventListener("click", function(){
-    document.getElementById("q4").style.display = "none";
-    document.getElementById("q5").style.display = "block";
-})
-
-document.getElementById("back06").addEventListener("click", function(){
-    document.getElementById("q4").style.display = "block";
-    document.getElementById("q5").style.display = "none";
-})
-
-document.getElementById("next06").addEventListener("click", function(){
-    document.getElementById("q5").style.display = "none";
-    document.getElementById("q6").style.display = "block";
-})
-
-document.getElementById("back07").addEventListener("click", function(){
-    document.getElementById("q5").style.display = "block";
-    document.getElementById("q6").style.display = "none";
-})
-
-document.getElementById("next07").addEventListener("click", function(){
-    document.getElementById("q6").style.display = "none";
-    document.getElementById("q7").style.display = "block";
-})
-
-document.getElementById("back08").addEventListener("click", function(){
-    document.getElementById("q6").style.display = "block";
-    document.getElementById("q7").style.display = "none";
-})
-
-document.getElementById("next08").addEventListener("click", function(){
-    document.getElementById("q7").style.display = "none";
-    document.getElementById("q8").style.display = "block";
-})
-
-document.getElementById("back09").addEventListener("click", function(){
-    document.getElementById("q7").style.display = "block";
-    document.getElementById("q8").style.display = "none";
-})
-
-document.getElementById("next09").addEventListener("click", function(){
-    document.getElementById("q8").style.display = "none";
-    document.getElementById("q9").style.display = "block";
-})
-
-document.getElementById("back010").addEventListener("click", function(){
-    document.getElementById("q8").style.display = "block";
-    document.getElementById("q9").style.display = "none";
-})
-
-
-document.getElementById("next010").addEventListener("click", function(){
-    document.getElementById("q9").style.display = "none";
-    document.getElementById("q10").style.display = "block";
-})
-
-document.getElementById("back011").addEventListener("click", function(){
-    document.getElementById("q9").style.display = "block";
-    document.getElementById("q10").style.display = "none";
-})
 
 
 
